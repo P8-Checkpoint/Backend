1145dee583c80fb6b2a4f326e4b7c9eb94648e18
Frederik <34657773+flja@users.noreply.github.com>
2023-04-25 13:48:04 +0200

    Update Program.cs

--- WorkrsBackend/WorkrsBackend/Program.cs
+++ WorkrsBackend/WorkrsBackend/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 using WorkrsBackend.Config;
 using WorkrsBackend.DataHandling;
 using WorkrsBackend.RabbitMQ;
 using Serilog;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using WorkrsBackend.DTOs;
 
 namespace WorkrsBackend
 {
@@ -59,6 +63,43 @@ namespace WorkrsBackend
             }
 
             //app.UseHttpsRedirection();
+
+            app.MapGet("/security/getMessage", () => "Hello World!").RequireAuthorization();
+            app.MapPost("/security/createToken",
+            [AllowAnonymous] (User user) =>
+            {
+                if (user.UserName == "joydip" && user.Password == "joydip123")
+                {
+                    var issuer = builder.Configuration["Jwt:Issuer"];
+                    var audience = builder.Configuration["Jwt:Audience"];
+                    var key = Encoding.ASCII.GetBytes
+                    (builder.Configuration["Jwt:Key"]);
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new[]
+                        {
+                new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti,
+                Guid.NewGuid().ToString())
+                         }),
+                        Expires = DateTime.UtcNow.AddMinutes(5),
+                        Issuer = issuer,
+                        Audience = audience,
+                        SigningCredentials = new SigningCredentials
+                        (new SymmetricSecurityKey(key),
+                        SecurityAlgorithms.HmacSha512Signature)
+                    };
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var token = tokenHandler.CreateToken(tokenDescriptor);
+                    var jwtToken = tokenHandler.WriteToken(token);
+                    var stringToken = tokenHandler.WriteToken(token);
+                    return Results.Ok(stringToken);
+                }
+                return Results.Unauthorized();
+            });
+
 
             app.UseAuthentication();
             app.UseAuthorization();
