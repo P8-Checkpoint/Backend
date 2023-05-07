cd399e2759e81bec0211d340089861819352c3fe
Frederik <34657773+flja@users.noreply.github.com>
2023-05-07 17:35:26 +0200

    started added locations

---
+++ WorkrsBackend/WorkrsBackend/Controllers/LocationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkrsBackend.DataHandling;
+using WorkrsBackend.DTOs;
+
+namespace WorkrsBackend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+
+    [Authorize]
+    public class LocationController : ControllerBase
+    {
+        ISharedResourceHandler _sharedResourceHandler;
+        public LocationController(ISharedResourceHandler sharedResourceHandler)
+        {
+            _sharedResourceHandler = sharedResourceHandler;
+        }
+
+        [HttpGet]
+        public ActionResult<LocationDTO> GetNearbyWorkers(LocationDTO locationDTO) 
+        {
+            return Ok(new LocationDTO() { Latitude = 0, Longitude = 0 });
+        }
+    }
+}
---
+++ WorkrsBackend/WorkrsBackend/DTOs/LocationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkrsBackend.DTOs
+{
+    public class LocationDTO
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
